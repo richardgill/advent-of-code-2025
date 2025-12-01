14ae2408577892229d8b43476e76d098fb34bb34
Richard Gill <richard@rgill.co.uk>
2025-12-01 11:42:24 +0000

    Implement day 1 solutions for Advent of Code 2025

--- src/days/01.1/index.ts
+++ src/days/01.1/index.ts
@@ -2,9 +2,27 @@ import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
+const parseLine = (line: string) => {
+  return {
+    direction: line.slice(0, 1),
+    numTurns: parseInt(line.slice(1, line.length), 10),
+  };
+};
+
 const solve = (input: string) => {
   const data = lines(input);
-  return data.length;
+  let dialNumber = 50;
+  let zeroCount = 0;
+  for (const line of lines(input)) {
+    const { direction, numTurns } = parseLine(line);
+    const addition = direction === "R" ? numTurns : -1 * numTurns;
+    dialNumber = (dialNumber + addition) % 100;
+
+    if (dialNumber === 0) {
+      zeroCount++;
+    }
+  }
+  return zeroCount;
 };
 
 console.log(solve(input));
--- src/days/01.2/index.ts
+++ src/days/01.2/index.ts
@@ -2,9 +2,34 @@ import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
+const parseLine = (line: string) => {
+  return {
+    direction: line.slice(0, 1),
+    numTurns: parseInt(line.slice(1, line.length), 10),
+  };
+};
+
 const solve = (input: string) => {
   const data = lines(input);
-  return data.length;
+  let dialNumber = 50;
+  let zeroCount = 0;
+  for (const line of lines(input)) {
+    const { direction, numTurns } = parseLine(line);
+    for (let i = 0; i < numTurns; i++) {
+      const addition = direction === "R" ? 1 : -1;
+      dialNumber += addition;
+      if (dialNumber === 100) {
+        dialNumber = 0;
+      }
+      if (dialNumber === -1) {
+        dialNumber = 99;
+      }
+      if (dialNumber === 0) {
+        zeroCount++;
+      }
+    }
+  }
+  return zeroCount;
 };
 
 console.log(solve(input));
