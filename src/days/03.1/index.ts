50130fcc6f42737b33db8c9854f2327088f7856d
Richard Gill <richard@rgill.co.uk>
2025-12-04 06:40:17 +0000

    Add day 3 solutions with input data

--- src/days/03.1/index.ts
+++ src/days/03.1/index.ts
@@ -1,10 +1,26 @@
+import { max, sum } from "es-toolkit/compat";
 import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
+const parseBanks = (input: string) => {
+  const banks = lines(input);
+  return banks.map((b) => b.split("").map((n) => parseInt(n, 10)));
+};
+
+const findHighest = (bank: number[]) => {
+  const highestForEachB = bank.slice(0, bank.length - 1).map((b, index) => {
+    const rightOfB = bank.slice(index + 1, bank.length);
+    const maxDigitRight = max(rightOfB) ?? -1;
+    return b * 10 + maxDigitRight;
+  });
+  return max(highestForEachB);
+};
+
 const solve = (input: string) => {
-  const data = lines(input);
-  return data.length;
+  const banks = parseBanks(input);
+  const highestBanks = banks.map((bank) => findHighest(bank));
+  return sum(highestBanks);
 };
 
 console.log(solve(input));
--- src/days/03.2/index.ts
+++ src/days/03.2/index.ts
@@ -1,10 +1,40 @@
+import { max, sum } from "es-toolkit/compat";
 import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
+const parseBanks = (input: string) => {
+  const banks = lines(input);
+  return banks.map((b) => b.split("").map((n) => parseInt(n, 10)));
+};
+
+const maxIndex = (numbers: number[]) => {
+  const highest = max(numbers);
+  return numbers.findIndex((n) => n === highest);
+};
+
+const findHighest = (bank: number[]) => {
+  let leftToDrop = bank.length - (12 - 1);
+  let currentIndex = 0;
+  let result = "";
+
+  while (result.length < 12) {
+    const nextIndex =
+      maxIndex(bank.slice(currentIndex, currentIndex + leftToDrop)) +
+      currentIndex;
+    leftToDrop -= nextIndex - currentIndex;
+    const nextValue = bank[nextIndex];
+    result = `${result}${nextValue}`;
+    currentIndex = nextIndex + 1;
+  }
+
+  return parseInt(result, 10);
+};
+
 const solve = (input: string) => {
-  const data = lines(input);
-  return data.length;
+  const banks = parseBanks(input);
+  const highestBanks = banks.map((bank) => findHighest(bank));
+  return sum(highestBanks);
 };
 
 console.log(solve(input));
