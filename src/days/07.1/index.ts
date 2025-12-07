d0b51aff30190a4b98202416d084f73d30dae71b
Richard Gill <richard@rgill.co.uk>
2025-12-07 08:48:21 +0000

    Add day 7 solutions and puzzle data

--- src/days/07.1/index.ts
+++ src/days/07.1/index.ts
@@ -1,10 +1,28 @@
+import { uniq } from "es-toolkit";
 import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
 const solve = (input: string) => {
-  const data = lines(input);
-  return data.length;
+  const data = lines(input).map((line) => line.split(""));
+  const firstLine = data[0];
+  if (!firstLine) throw new Error("no first line");
+  let beamIndexes = [firstLine.findIndex((c) => c === "S")];
+  let splitCount = 0;
+  for (const line of data.slice(1)) {
+    beamIndexes = uniq(
+      beamIndexes.flatMap((index) => {
+        if (line[index] === ".") {
+          return index;
+        } else if (line[index] === "^") {
+          splitCount++;
+          return [index - 1, index + 1];
+        }
+        throw new Error("unknown char");
+      }),
+    );
+  }
+  return splitCount;
 };
 
 console.log(solve(input));
--- src/days/07.2/index.ts
+++ src/days/07.2/index.ts
@@ -1,10 +1,36 @@
+import { groupBy, sumBy } from "es-toolkit";
 import { lines } from "../../lib/utils";
 
 const input = await Bun.file(import.meta.dir + "/data/input.txt").text();
 
 const solve = (input: string) => {
-  const data = lines(input);
-  return data.length;
+  const data = lines(input).map((line) => line.split(""));
+  const firstLine = data[0];
+  if (!firstLine) throw new Error("no first line");
+  let beamIndexes = [
+    { index: firstLine.findIndex((c) => c === "S"), beamCount: 1 },
+  ];
+  for (const line of data.slice(1)) {
+    const allBeamIndexes = beamIndexes.flatMap(({ index, beamCount }) => {
+      if (line[index] === ".") {
+        return { index, beamCount };
+      } else if (line[index] === "^") {
+        return [
+          { index: index - 1, beamCount },
+          { index: index + 1, beamCount },
+        ];
+      }
+      throw new Error("unknown char");
+    });
+    const grouped = Object.entries(
+      groupBy(allBeamIndexes, (item) => item.index),
+    );
+    beamIndexes = grouped.map(([_drop, beams]) => ({
+      index: beams[0]?.index ?? -1,
+      beamCount: sumBy(beams, (b) => b.beamCount),
+    }));
+  }
+  return sumBy(beamIndexes, ({ beamCount }) => beamCount);
 };
 
 console.log(solve(input));
